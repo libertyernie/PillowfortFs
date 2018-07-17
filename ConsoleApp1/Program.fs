16ef23225a9927c3c8a47aa1ba3fe84dcfb11586
libertyernie <maybeway36@gmail.com>
2018-07-17 07:56:28 -0500

    Start working on pillowfort.io client

---
+++ ConsoleApp1/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let c = PillowfortFs.PillowfortClientFactory.login "user@example.com" "password" |> Async.RunSynchronously
+    printfn "%s" c.Cookie
+    0 // return an integer exit code
---
+++ PillowfortFs/PillowfortClient.fs
@@ -0,0 +1,12 @@
+﻿namespace PillowfortFs
+
+open System
+open System.Net
+
+type PillowfortClient() =
+    let cookies = new CookieContainer()
+    let cookie_wrapper = new SingleCookieWrapper(cookies, new Uri("https://pillowfort.io"), "_Pillowfort_session")
+
+    member __.Cookie
+        with get() = cookie_wrapper.getCookieValue() |> Option.defaultValue null
+        and set (v: string) = cookie_wrapper.setCookieValue <| Option.ofObj v
